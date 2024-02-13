42ef57825b04d09a00e3d97ab3f56e3f9ca4b0d9
BaguetteYeeter <baguetteyeeter@icloud.com>
2024-02-13 17:18:03 +0000

    it now prints the terminal size

--- src/main.c
+++ src/main.c
@@ -2,10 +2,21 @@
 #include <ncurses.h>
 
 int main() {
+    int row,col;
+
     initscr();
-    printw("Hello world!");
+
+    getmaxyx(stdscr,row,col);
+
+    raw();
+    noecho();
+    keypad(stdscr, TRUE);
+
+    printw("Hello world!\n");
+    printw("The terminal is %dx%d", row, col);
     refresh();
     getch();
+    
     endwin();
     return 0;
 }
