e081fafaeb58a1d7869e2667fe0d9db7a7dd23fb
BaguetteYeeter <baguetteyeeter@icloud.com>
2024-02-14 15:00:51 +0000

    now gets key input

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 
 int main() {
     int row,col;
+    int ch;
 
     initscr();
 
@@ -11,11 +12,22 @@ int main() {
     raw();
     noecho();
     keypad(stdscr, TRUE);
+    cbreak();
+    timeout(1000);
 
     printw("Hello world!\n");
     printw("The terminal is %dx%d", row, col);
     refresh();
-    getch();
+
+    while (true) {
+        ch = getch();
+        if (ch != ERR) {
+            if (ch == KEY_F(1)) {
+                break;
+            }
+            printw("%d\n", ch);
+        }
+    }
     
     endwin();
     return 0;
