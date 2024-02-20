b8c7450e7876e4c980ee34a34d2fc0454ae3b1d6
BaguetteYeeter <baguetteyeeter@icloud.com>
2024-02-20 19:01:00 +0000

    changed timing system (too much effort)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <time.h>
 
 typedef struct {
     int x;
@@ -75,11 +76,43 @@ void update(pointer op, node *points) {
     refresh();
 }
 
+//thank you stack overflow
+bool valueinarray(int val, int *arr, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        if(arr[i] == val)
+            return true;
+    }
+    return false;
+}
+
+void addtoarray(int val, int *arr, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        if(arr[i] == -1) {
+            arr[i] = val;
+            break;
+        }
+    }
+}
+
+void cleararray(int *arr, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        arr[i] = -1;
+    }
+}
+
 int main() {
     int row,col;
     int ch;
     enum Directions dir = RIGHT;
-    bool moved;
+    enum Directions lastdir = RIGHT;
+    bool moved, automoved;
+
+    struct timespec time1, time2;
+    double timetaken;
+    int delay = 20;
+    int keys[10];
+    cleararray(keys, 10);
+    
 
     pointer p;
     p.x = 0;
@@ -117,10 +150,10 @@ int main() {
 
     raw();
     noecho();
-    keypad(stdscr, TRUE);
     cbreak();
+    keypad(stdscr, TRUE);
     curs_set(0);
-    timeout(200);
+    timeout(delay);
 
     printw("Hello world!\n");
     printw("The terminal is %dx%d", row, col);
@@ -128,69 +161,106 @@ int main() {
 
     update(op, points);
 
+    timetaken = 0;
+
     while (true) {
+        //mvprintw(5, 0, "1 "); refresh();
         moved = false;
+        automoved = false;
+
+        clock_gettime(CLOCK_MONOTONIC, &time1);
         ch = getch();
-        if (ch != ERR) {
-            if (ch == KEY_F(1)) {
-                break;
-            } else if (ch == KEY_RESIZE) {
-                getmaxyx(stdscr,row,col);
-                mvprintw(1, 0, "The terminal is %dx%d", row, col);
-            }
+        clock_gettime(CLOCK_MONOTONIC, &time2);
 
-            p = array_last(points);
+        //mvprintw(2, 0, "%d       ", 1000*(time2-time1)/CLOCKS_PER_SEC); refresh();
 
-            if (ch == KEY_UP && p.y > 0) {
-                p.y--;
-                dir = UP;
-                moved = true;
-            } else if (ch == KEY_DOWN && p.y < row-1) {
-                p.y++;
-                dir = DOWN;
-                moved = true;
-            } else if (ch == KEY_LEFT && p.x > 0) {
-                p.x--;
-                dir = LEFT;
-                moved = true;
-            } else if (ch == KEY_RIGHT && p.x*2 < col-2) {
-                p.x++;
-                dir = RIGHT;
-                moved = true;
-            }
-
-            if (moved) {
-                op = array_pop(&points);
-                array_push(points, p);
-            }
-
-            update(op,points);
-
-            //printw("%d\n", ch);
-        } else {
-            p = array_last(points);
-
-            if (dir == UP && p.y > 0) {
-                p.y--;
-                moved = true;
-            } else if (dir == DOWN && p.y < row-1) {
-                p.y++;
-                moved = true;
-            } else if (dir == LEFT && p.x > 0) {
-                p.x--;
-                moved = true;
-            } else if (dir == RIGHT && p.x*2 < col-2) {
-                p.x++;
-                moved = true;
-            }
-
-            if (moved) {
-                op = array_pop(&points);
-                array_push(points, p);
-            }
-
-            update(op, points);
+        if (!valueinarray(ch, keys, 10) && ch != ERR) {
+            addtoarray(ch, keys, 10);
         }
+
+
+        double elapsed = (time2.tv_sec - time1.tv_sec) + (time2.tv_nsec - time1.tv_nsec) / 1e9;
+
+        timetaken += elapsed;
+
+        if (timetaken*1000 < delay*10) {
+            continue;
+        }
+
+        timetaken = 0;
+
+        for (int i = 0; i < 10; i++) {
+            ch = keys[i];
+
+            if (ch != ERR) {
+                if (ch == KEY_F(1)) {
+                    break;
+                } else if (ch == KEY_RESIZE) {
+                    getmaxyx(stdscr,row,col);
+                    mvprintw(1, 0, "The terminal is %dx%d", row, col);
+                }
+
+                p = array_last(points);
+
+                if (ch == KEY_UP && p.y > 0) {
+                    p.y--;
+                    dir = UP;
+                    lastdir = UP;
+                    moved = true;
+                } else if (ch == KEY_DOWN && p.y < row-1) {
+                    p.y++;
+                    dir = DOWN;
+                    lastdir = DOWN;
+                    moved = true;
+                } else if (ch == KEY_LEFT && p.x > 0) {
+                    p.x--;
+                    dir = LEFT;
+                    lastdir = LEFT;
+                    moved = true;
+                } else if (ch == KEY_RIGHT && p.x*2 < col-2) {
+                    p.x++;
+                    dir = RIGHT;
+                    lastdir = RIGHT;
+                    moved = true;
+                }
+
+                if (moved) {
+                    op = array_pop(&points);
+                    array_push(points, p);
+                }
+
+                update(op,points);
+
+                //printw("%d\n", ch);
+            } else if (!automoved && !moved) {
+                automoved = true;
+
+                p = array_last(points);
+
+                if (dir == UP && p.y > 0) {
+                    p.y--;
+                    moved = true;
+                } else if (dir == DOWN && p.y < row-1) {
+                    p.y++;
+                    moved = true;
+                } else if (dir == LEFT && p.x > 0) {
+                    p.x--;
+                    moved = true;
+                } else if (dir == RIGHT && p.x*2 < col-2) {
+                    p.x++;
+                    moved = true;
+                }
+
+                if (moved) {
+                    op = array_pop(&points);
+                    array_push(points, p);
+                }
+
+                update(op, points);
+            }
+        }
+
+        cleararray(keys, 10);
     }
     
     endwin();
